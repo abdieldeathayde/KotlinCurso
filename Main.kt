0f6eae3360c2963605ff9a48f4064eea6edb8097
Abdiel Athayde <46298069+abdieldeathayde@users.noreply.github.com>
2023-08-21 20:53:49 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+    println(123)
+    println(3 + 2)
+    println(3 - 2)
+    println(3 * 2)
+    prinln(3 % 2)
+
+    println(3.0 / 2.0)
+
+    println("%.2f".format(29.0 / 10.0))
+
+
+
+}
