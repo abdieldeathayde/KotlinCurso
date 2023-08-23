11838b7a12fb650e161c2caf304aafba2246ada2
Abdiel <abdieldeathayde67@gmail.com>
2023-08-23 19:42:20 -0300

    atualizando curso Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,39 @@
+import kotlin.math.*
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello Kotlin!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+    //forever()
+
+    var sun = 1
+
+//    do {
+//        println(sun)
+//        sun += 1
+//    } while (sun < 1000)
+
+    while (sun < 1000) {
+        sun += 1
+        println(sun)
+        if (sun == 500)
+            break
+    }
 }
+
+//fun add(): Unit {
+//    val result = 2 + 2
+//    println(result)
+//}
+
+fun forever(): Nothing {
+    while (true) {
+        Thread.sleep(1000)
+        println("oi")
+    }
+}
+
+
+
